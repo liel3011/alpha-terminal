6763e4de693d713aaf400e0b631fb5fa5d35c2b8
liel3011 <72576438+liel3011@users.noreply.github.com>
2026-05-02 12:29:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ except ImportError as e:
 
 # --- INITIALIZATION ---
 load_dotenv()
-st.set_page_config(page_title="Aglo Trader Terminal", layout="wide", page_icon="⚡", initial_sidebar_state="collapsed")
+st.set_page_config(page_title="Aglo Trader Terminal", layout="wide", page_icon="🪙", initial_sidebar_state="collapsed")
 db = DatabaseManager()
 
 # --- HIGH-END PROFESSIONAL CSS ---
